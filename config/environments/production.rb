295da12ab5a9ba861fe490bc23d0bbfb5a7d8ad3
Michael Giardina <mgiardi0@bu.edu>
2018-04-16 13:41:33 -0400

    Fixed production bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
       :user_name        => ENV['SENDGRID_USERNAME'],
       :password         => ENV['SENDGRID_PASSWORD'],
       :DOMAIN           => 'heroku.com'
-      :enable_starttls_auto  =>true
+      :enable_starttls_auto  => true
   }
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
