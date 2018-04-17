7bf1bc3ac1db06c12274fe8b0e674d3734c898b8
Michael Giardina <mgiardi0@bu.edu>
2018-04-17 01:21:51 -0400

    Fix activation mailer issue

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      if user.authenticated?
+      if user.activated?
         log_in user
         params[:session][:remember_me] == '1' ? remember(user) : forget(user)
         redirect_back_or user
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
         flash[:warning] = message
         redirect_to root_url
       end
-    elser
+    else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token, :reset_token
+  attr_accessor :remember_token, :activation_token
   before_save :downcase_email
   before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
@@ -30,7 +30,7 @@ class User < ApplicationRecord
 
   # Returns true if the given token mathes the digest.
   def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
+    digest =send ("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
@@ -43,29 +43,12 @@ class User < ApplicationRecord
   # Activates an account
   def activate
     update_attribute(:activated, true)
-    update_attribute(activated_at, Time.zone.now)
+    update_attribute(:activated_at, Time.zone.now)
   end
 
   # Sends activation email
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
-  end
-#end
-
-  # Sets the password reset attributes
-  def create_reset_digest
-    self.reset_token = User.new_token
-    update_attribute(:reset_digest, User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
-  end
-
-  # Sends password reset email.
-  def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
-  end
-
-  def password_reset_expired?
-    reset_sent_at < 2.hours.ago
   end
 
   private
